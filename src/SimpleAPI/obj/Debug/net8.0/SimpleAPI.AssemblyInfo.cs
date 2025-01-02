a29a3512472b4a5f0a243bfd4aebd45d3386a2d5
RahatOnGit <rahat1803098@gmail.com>
2025-01-02 11:17:54 +0600

    Another

--- src/SimpleAPI/obj/Debug/net8.0/SimpleAPI.AssemblyInfo.cs
+++ src/SimpleAPI/obj/Debug/net8.0/SimpleAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c76c28faa287a6e39ccab60cbd40d3b37c6bc50")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
